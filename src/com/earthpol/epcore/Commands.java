156b22219433982b0d9b23620b483e7418536629
0xBit <73406588+0xBit-MC@users.noreply.github.com>
2020-12-03 00:04:08 -0800

    Custom Configuration Addition.
    These changes push forward items for a custom configuration file called "options.yml" that is generated on start-up with defaults.

---
+++ src/com/earthpol/epcore/Commands.java
@@ -0,0 +1,5 @@
+package com.earthpol.epcore;
+
+public class Commands {
+
+}
--- src/com/earthpol/epcore/Main.java
+++ src/com/earthpol/epcore/Main.java
@@ -1,31 +1,38 @@
 package com.earthpol.epcore;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	public static Main instance;
 	public static Logger log = Bukkit.getLogger();
-	public static PluginDescriptionFile pluginFile;
-	String version = pluginFile.getVersion();
-	List<String> authors = pluginFile.getAuthors();
-	String website = pluginFile.getWebsite();
 	
 	@Override
 	public void onEnable() {
 		instance = this;
 		log.info("======= EPMC CORE =======");
 		log.info("= EarthPol Core Plugin");
-		log.info("= Version: " + version);
-		log.info("= Authors: " + authors);
-		log.info("= Website: " + website);
+		log.info("= Version: " + this.getDescription().getVersion());
+		log.info("= Authors: " + this.getDescription().getAuthors());
+		log.info("= Website: " + this.getDescription().getWebsite());
 		log.info("= Support: https://discord.gg/DvtZzztAfF");
 		log.info("=========================");
+		
+		//Setup Configuration File.
+		getConfig().options().copyDefaults();
+		saveDefaultConfig();
+		
+		//Setup Custom Configuration File and Load it.
+		Config.setup();
+		
+		//Get the configuration file.
+		Config.getConfig();
+		
+		//Save the configuration File.
+		Config.saveConfig();
 	}
 	
 	@Override
